fd6127505dd997297f8067c70b9012530f9fd166
Salvator Guarnera <salvatorguarnera@gmail.com>
2020-07-26 15:07:25 -0500

    Added skills to skills grid

--- src/components/SkillGrid/skillgrid.js
+++ src/components/SkillGrid/skillgrid.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from './skillgrid.module.css';
-import { Grid } from '@material-ui/core';
+import { Grid, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 
 export class SkillGrid extends Component {
 	render() {
@@ -12,28 +12,90 @@ export class SkillGrid extends Component {
 					<div className={styles.flexItemOne}>
 						<Grid container direction="column" justify="flex-start">
 							<Grid item>
-								<h3>Header</h3>
+								<h3>Front End Experience</h3>
 							</Grid>
 							<Grid item>
-								<ul>
-									<li>
-										<p>Item One</p>
-									</li>
-									<li>
-										<p>Item Two</p>
-									</li>
-									<li>
-										<p>Item Three</p>
-									</li>
-								</ul>
-							</Grid>
-							<Grid item>
-								<p>A short description about this skill set</p>
+								<List>
+									<ListItem>
+										<p>React</p>
+									</ListItem>
+									<ListItem>
+										<p>React Redux</p>
+									</ListItem>
+									<ListItem>
+										<p>React Router</p>
+									</ListItem>
+									<ListItem>
+										<p>Javascript</p>
+									</ListItem>
+									<ListItem>
+										<p>TypeScript</p>
+									</ListItem>
+									<ListItem>
+										<p>HTML, CSS, SASS</p>
+									</ListItem>
+								</List>
 							</Grid>
 						</Grid>
 					</div>
-					<div className={styles.flexItemOne} />
-					<div className={styles.flexItemOne} />
+					<div className={styles.flexItemOne}>
+						<Grid container direction="column" justify="flex-start">
+							<Grid item>
+								<h3>Full Stack Experience</h3>
+							</Grid>
+							<Grid item>
+								<List>
+									<ListItem>
+										<p>Swift, Objective-C</p>
+									</ListItem>
+									<ListItem>
+										<p>Java, C#</p>
+									</ListItem>
+									<ListItem>
+										<p>Python</p>
+									</ListItem>
+									<ListItem>
+										<p>Node.js</p>
+									</ListItem>
+									<ListItem>
+										<p>Firebase, Firestore, Cloud Functions</p>
+									</ListItem>
+									<ListItem>
+										<p>AWS Lambda, DynamoDB, S3</p>
+									</ListItem>
+									<ListItem>
+										<p>Docker, Git, CI/CD</p>
+									</ListItem>
+								</List>
+							</Grid>
+						</Grid>
+					</div>
+					<div className={styles.flexItemOne}>
+						<Grid container direction="column" justify="flex-start">
+							<Grid item>
+								<h3>Interesting Courses I've Completed</h3>
+							</Grid>
+							<Grid item>
+								<List>
+									<ListItem>
+										<p>Foundations of AI</p>
+									</ListItem>
+									<ListItem>
+										<p>Cryptology</p>
+									</ListItem>
+									<ListItem>
+										<p>Blockchains and Smart Contracts</p>
+									</ListItem>
+									<ListItem>
+										<p>Theory of Numbers</p>
+									</ListItem>
+									<ListItem>
+										<p>Wonders, Cons, and Scandals (Conspiracy theory class)</p>
+									</ListItem>
+								</List>
+							</Grid>
+						</Grid>
+					</div>
 				</div>
 			</div>
 		);
